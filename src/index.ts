926081a4e241d0ea180310353956190ebae5dc36
minatisleeping <minatt2002@gmail.com>
2024-05-03 16:27:39 +0700

    add file..

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import express from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 
 const app = express()
 const PORT = process.env.PORT
 databaseService.connect()
-
 app.use(express.json())
 
+// Tạo folder
+initFolder()
+
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 
 app.use(defaultErrorHandler)
 
