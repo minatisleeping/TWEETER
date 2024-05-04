331667ccfd7bce87e8788c1f553c12c65994156a
minatisleeping <minatt2002@gmail.com>
2024-05-04 12:55:49 +0700

    handle image with sharp..

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from 'express'
+import { Request } from 'express'
 import fs from 'fs'
 import path from 'path'
-import formidable from 'formidable'
+import formidable, { File } from 'formidable'
+import { UPLOAD_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
-  const uploadFolderPath = path.resolve('uploads')
-  if (!fs.existsSync(uploadFolderPath)) {
-    fs.mkdirSync(uploadFolderPath, { recursive: true })
+  if (!fs.existsSync(UPLOAD_TEMP_DIR)) {
+    fs.mkdirSync(UPLOAD_TEMP_DIR, { recursive: true })
   }
 }
 
 export const handleUploadSingleImage = async (req: Request) => {
   // const formidable = (await import('formidable')).default
   const form = formidable({
-    uploadDir: path.resolve('uploads'),
+    uploadDir: UPLOAD_TEMP_DIR,
     maxFiles: 1,
     keepExtensions: true,
     maxFileSize: 3000 * 1024,
@@ -27,14 +27,20 @@ export const handleUploadSingleImage = async (req: Request) => {
     }
   })
 
-  return new Promise((resolve, reject) => {
+  return new Promise<File>((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
       if (err) return reject(err)
 
       // * vì files.image là undefined nên phải chuyển về boolean để kiểm tra
       if (!files.image) return reject(new Error('File is empty'))
 
-      resolve(files)
+      resolve((files.image as File[])[0])
     })
   })
+}
+
+export const getNameFromFullname = (fullname: string) => {
+  const namearr = fullname.split('.')
+  namearr.pop()
+  return namearr.join('')
 }
