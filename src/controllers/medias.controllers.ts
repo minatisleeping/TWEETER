70d78f8e2f4f918c65dfe656d26eb05152911b54
minatisleeping <minatt2002@gmail.com>
2024-05-04 10:46:09 +0700

    filter upload image..

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,20 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
-import formidable from 'formidable'
-import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  // const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024 // 300KB
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) throw err
+  const data = await handleUploadSingleImage(req)
 
-    res.json({ message: 'Upload image successfully!' })
-  })
-
-  // res.json({ message: 'cc' })
+  return res.json({ result: data })
 }
--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { uploadSingleImageController } from '~/controllers/medias.controllers'
+import { wrapReqHandler } from '../utils/handler'
 
 const mediasRouter = Router()
 
-mediasRouter.post('/upload-image', uploadSingleImageController)
+mediasRouter.post('/upload-image', wrapReqHandler(uploadSingleImageController))
 
 export default mediasRouter
