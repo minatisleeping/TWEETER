20ae5eb6fc06b239e731e11fe190cf7d2781395f
minatisleeping <minatt2002@gmail.com>
2024-05-08 07:39:41 +0700

    handle para from command, split into 2 env, one for dev, one for production..

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,16 @@ import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
+import argv from 'minimist'
+
+const options = argv(process.argv.slice(2))
 
 const app = express()
 const PORT = process.env.PORT
 databaseService.connect()
 app.use(express.json())
 
+console.log(options)
 // Tạo folder
 initFolder()
 
@@ -20,5 +24,5 @@ app.use('/medias', mediasRouter)
 app.use(defaultErrorHandler)
 
 app.listen(PORT, () => {
-  console.log(`DUTHANHDUOC - Nodejs is running at ${process.env.DB_HOST}:${PORT}`)
+  console.log(`DUTHANHDUOC - Nodejs is running at ${process.env.LOCALHOST}:${PORT}`)
 })
