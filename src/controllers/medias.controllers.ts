7f65d2769abacfebcc543d653930b4f3a2a82f3d
minatisleeping <minatt2002@gmail.com>
2024-05-08 12:14:20 +0700

    serving static file in another way..

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import path from 'path'
+import { UPLOAD_DIR } from '~/constants/dir'
 import { USER_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 
@@ -8,5 +11,16 @@ export const uploadSingleImageController = async (req: Request, res: Response, n
   return res.json({
     message: USER_MESSAGES.UPLOAD_SUCCESS,
     result: url
+  })
+}
+
+export const serveImageController = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  console.log('🚀 ~ serveImageController ~ name:', name)
+
+  return res.sendFile(path.resolve(UPLOAD_DIR, name + '.jpg'), (err) => {
+    if (err) {
+      return res.status((err as any).status).send('Not found')
+    }
   })
 }
