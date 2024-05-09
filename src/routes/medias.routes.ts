afe779c4f97efa9ee4251b1fa2f8dff5adecd3b0
minatisleeping <minatt2002@gmail.com>
2024-05-09 09:12:02 +0700

    upload video..

--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express'
-import { uploadImageController } from '~/controllers/medias.controllers'
+import { uploadImageController, uploadVideoController } from '~/controllers/medias.controllers'
 import { wrapReqHandler } from '../utils/handler'
+import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 
 const mediasRouter = Router()
 
-mediasRouter.post('/upload-image', wrapReqHandler(uploadImageController))
+mediasRouter.post(
+  '/upload-image',
+  accessTokenValidator,
+  verifiedUserValidator as any,
+  wrapReqHandler(uploadImageController)
+)
+
+mediasRouter.post(
+  '/upload-video',
+  accessTokenValidator,
+  verifiedUserValidator as any,
+  wrapReqHandler(uploadVideoController)
+)
 
 export default mediasRouter
