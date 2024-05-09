90c743015ee5207171615ada1547da162e5d363f
minatisleeping <minatt2002@gmail.com>
2024-05-09 08:38:55 +0700

    upload multiple images..

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -5,8 +5,8 @@ import { UPLOAD_DIR } from '~/constants/dir'
 import { USER_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 
-export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const url = await mediasService.handleUploadSingleImage(req)
+export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const url = await mediasService.uploadImage(req)
 
   return res.json({
     message: USER_MESSAGES.UPLOAD_SUCCESS,
--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { uploadSingleImageController } from '~/controllers/medias.controllers'
+import { uploadImageController } from '~/controllers/medias.controllers'
 import { wrapReqHandler } from '../utils/handler'
 
 const mediasRouter = Router()
 
-mediasRouter.post('/upload-image', wrapReqHandler(uploadSingleImageController))
+mediasRouter.post('/upload-image', wrapReqHandler(uploadImageController))
 
 export default mediasRouter
