c9f4e021fa0e9fc1c8904fd4c03736cad14268db
minatisleeping <minatt2002@gmail.com>
2024-05-10 12:40:21 +0700

    Streaming video..

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
-import { UPLOAD_IMAGE_DIR } from './constants/dir'
+import { UPLOAD_VIDEO_DIR } from './constants/dir'
 import staticRouter from './routes/static.routes'
 
 const app = express()
@@ -20,7 +20,7 @@ app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
 app.use('/static', staticRouter)
 
-// app.use('/static', express.static(UPLOAD_IMAGE_DIR))
+app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
 
 app.use(defaultErrorHandler)
 
